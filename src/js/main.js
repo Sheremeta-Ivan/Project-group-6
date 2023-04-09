427a74e838d240da87cfa1d282acca7623eea2cd
MarianaNew <maryanatiurina@gmail.com>
2023-04-09 16:17:53 +0100

    Modal

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import './review.js';
 import './buy-now-modal.js';
 import './modal-leave-a-review.js';
 import './modal-buy-now-click.js';
-
-
+import './modal-subscribe.js';
 
 import './mobile-menu.js';
